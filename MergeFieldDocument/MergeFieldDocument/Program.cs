97645988b377109189b98a2a6a4b66b6a79cc2fa
Micheal Boyle <michealboyle@outlook.com>
2023-03-29 09:46:21 -0400

    Change to use openxml

--- MergeFieldDocument/MergeFieldDocument/Program.cs
+++ MergeFieldDocument/MergeFieldDocument/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MergeFieldDocument
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WordMerge
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string jsonFile = args[0];
+            string templateDocument = args[1];
+            string outputDocument = args[2];
+
+            Dictionary<string, string> mergeFields = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(jsonFile));
+
+            using (WordprocessingDocument doc = WordprocessingDocument.Create(outputDocument, WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                using (WordprocessingDocument templateDoc = WordprocessingDocument.Open(templateDocument, false))
+                {
+                    var docFields = templateDoc.MainDocumentPart.Document.Descendants<FieldCode>();
+                    foreach (var field in docFields)
+                    {
+                        string fieldName = field.Text.Replace("MERGEFIELD ", "").Trim();
+                        if (mergeFields.ContainsKey(fieldName))
+                        {
+                            string fieldValue = mergeFields[fieldName];
+                            field.Parent.ReplaceChild(new SimpleField(new Run(new DocumentFormat.OpenXml.Drawing.Text(fieldValue))), field);
+                        }
+                    }
+                }
+
+                mainPart.Document.Save();
+            }
+
+            Console.WriteLine("Merge complete");
         }
     }
 }
